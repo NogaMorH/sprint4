b40aeaaf55ecc0fcd9b1dfd489a99131772b1a14
kfirazu <kfirazu@gmail.com>
2022-09-15 17:15:49 +0300

    Handled conflicts

--- src/cmps/board/task-preview.jsx
+++ src/cmps/board/task-preview.jsx
@@ -1,7 +1,14 @@
-export const TaskPreview = () => {
+
+
+export const TaskPreview = ({task}) => {
     return (
-        <div>
-            task preview
-        </div>
+        <ul>
+            <li key={task.id}>
+                <h4>{task.title}</h4>
+                {/* <span>{formatTime(task.dueDate)}</span> */}
+                {/* {<div>{task.memberIds.map(memberId => memberId)}</div> */}
+
+            </li>
+        </ul>
     )
 }
