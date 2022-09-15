023fa2aede703bcf5fe716e316f98b8e81b4a103
Noga Mor-Havilio <nogamorh@gmail.com>
2022-09-15 21:40:58 +0300

    merge

--- src/cmps/board/group-preview.jsx
+++ src/cmps/board/group-preview.jsx
@@ -4,6 +4,7 @@ export const GroupPreview = ({ group }) => {
     return (
         <div className='group-preview flex column'>
             <h3>{group.title}</h3>
+            {/* <button>edit</button> */}
             <TaskList tasks={group.tasks} />
         </div>
     )
