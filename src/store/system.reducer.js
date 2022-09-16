74b4407f13fb2bdef3b3d42e28bf32efbb17d048
Noga Mor-Havilio <nogamorh@gmail.com>
2022-09-16 21:59:47 +0300

    edit group title

--- src/store/system.reducer.js
+++ src/store/system.reducer.js
@@ -5,7 +5,8 @@ const initialState = {
     groupId: null,
     isAddGroup: false
   },
-  modalGroupId: null
+  modalGroupId: null,
+  titleGroupId: null
 }
 
 export function systemReducer(state = initialState, action = {}) {
@@ -14,8 +15,10 @@ export function systemReducer(state = initialState, action = {}) {
       return { ...state, formAdd: { groupId: action.groupId } }
     case 'SET_FORM_ADD_IS_ADD_GROUP':
       return { ...state, formAdd: { isAddGroup: action.isAddGroup } }
-    case 'SET_IS_GROUP_MODAL_OPEN':
-      return { ...state, modalGroupId: action.groupId }
+    case 'SET_MODAL_GROUP_ID':
+      return { ...state, modalGroupId: action.groupIdToDispatch }
+    case 'SET_TITLE_GROUP_ID':
+      return { ...state, titleGroupId: action.groupId }
     //   case 'LOADING_START':
     //     return { ...state, isLoading: true }
     //   case 'LOADING_DONE':
