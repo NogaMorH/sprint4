7e3d67ace50b549d8e406c29319c430d0f73a24f
Rebecca Kruk <rebeccakruk@gmail.com>
2022-09-22 08:48:16 +0300

    todo cmps functionlity + style

--- src/cmps/task-details/todo-list.jsx
+++ src/cmps/task-details/todo-list.jsx
@@ -1,39 +1,32 @@
-import { useEffect, useState } from "react"
 import { utilService } from "../../services/util.service"
 import { Todo } from "./todo"
 
 export const TodoList = ({ todos, updateChecklist }) => {
 
-    const [currTodos, setTodos] = useState(todos)
-
-    useEffect(() => {
-        updateChecklist('todos', currTodos)
-    }, [currTodos])
-
     const updateTodos = (todo) => {
-        setTodos(prevTodos => (
-            prevTodos.map(currTodo => {
-                if (currTodo.id === todo.id) {
-                    return todo
-                }
-                return currTodo
-            })
-        ))
+        const updatedTodos = todos.map(currTodo => {
+            if (currTodo.id === todo.id) {
+                return todo
+            }
+            return currTodo
+        })
+        updateChecklist('todos', updatedTodos)
     }
 
     const addTodo = () => {
-        setTodos(prevTodos => ([...prevTodos, { id: utilService.makeId(), title: '', isDone: false }]))
+        todos.push({ id: utilService.makeId(), title: '', isDone: false })
+        updateChecklist('todos', todos)
     }
 
     const removeTodo = (todoId) => {
-        const updatedTodos = currTodos.filter(currTodo => currTodo.id !== todoId)
-        setTodos(updatedTodos)
+        const updatedTodos = todos.filter(currTodo => currTodo.id !== todoId)
+        updateChecklist('todos', updatedTodos)
     }
 
     return (
         <div className="todo-list-container">
             <ul className="todo-list">
-                {currTodos.map(todo => (
+                {todos.map(todo => (
                     <Todo key={todo.id} todo={todo} updateTodos={updateTodos} removeTodo={removeTodo} />
                 ))}
             </ul>
--- src/cmps/task-details/todo.jsx
+++ src/cmps/task-details/todo.jsx
@@ -1,25 +1,58 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
+import { IoCloseOutline } from 'react-icons/io5'
 
 export const Todo = ({ todo, updateTodos, removeTodo }) => {
 
-    const [currTodo, setTodo] = useState(todo)
-    let { id, title, isDone } = currTodo
+    let { id, title, isDone } = todo
+    const [titleField, setTitleField] = useState(title)
+    const [focused, setFocused] = useState(false)
 
-    useEffect(() => {
-        updateTodos(currTodo)
-    }, [currTodo])
+    const onFocus = () => setFocused(true)
+
+    const onBlur = (ev) => {
+        if (!ev.relatedTarget) {
+            setTitleField(title)
+            console.log('Checklist title canceled!')
+        }
+        else if (ev.relatedTarget.className === 'todo-save-btn') {
+            todo = { ...todo, title: titleField }
+            updateTodos(todo)
+            console.log('Checklist title saved!')
+        }
+        setFocused(false)
+    }
 
     const handleChange = ({ target }) => {
         let { name, value } = target
-        if (name === 'isDone') value = !isDone
-        setTodo(prevTodo => ({ ...prevTodo, [name]: value }))
+        if (name === 'isDone') {
+            todo = { ...todo, isDone: !isDone }
+            updateTodos(todo)
+        } else {
+            setTitleField(value)
+        }
     }
 
     return (
         <li className="todo">
             <input type="checkbox" name="isDone" onChange={handleChange} checked={isDone} />
-            <input type="text" name="title" placeholder="Todo..." value={title} onChange={handleChange} />
-            <button className="todo-remove-btn" onClick={() => removeTodo(id)}>x</button>
+
+            <div className="todo">
+                <textarea className="todo-textarea"
+                    value={titleField}
+                    onChange={handleChange}
+                    onFocus={onFocus}
+                    onBlur={onBlur}
+                />
+
+                {!focused && <button className="todo-remove-btn" onClick={() => removeTodo(id)}>Delete</button>}
+
+                {focused &&
+                    <div className="todo-buttons">
+                        <button className="todo-save-btn">Save</button>
+                        <span><IoCloseOutline /></span>
+                    </div>
+                }
+            </div>
         </li>
     )
 }
